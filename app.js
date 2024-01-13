f601f1bfc4f0ddbb92e41fb7bda3540c476a0901
JSJ <jsj950611@naver.com>
2024-01-13 19:04:13 +0900

    config

--- app.js
+++ app.js
@@ -1,12 +1,54 @@
 const express = require("express");
 const app = express();
+const routers = require('./routers/router')
 
-app.set("views", __dirname+'/views');
+const path = require('path')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const nunjucks = require('nunjucks')
+const dotenv = require('dotenv');
+const fs = require('fs')
+
+dotenv.config();
+app.use(morgan('dev'))
 app.set("view engine", "ejs");
-app.set(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'/public')))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(session({
+  resave : false,
+  saveUninitialized : false,
+  secret : process.env.COOKIE_SECRET,
+  cookie : {
+    httpOnly : true,
+    secure : false,    
+  }
+}))
 
-app.use("/", function (req, res) {
-  res.render("main");
-});
+
+app.use
+
+
+
+app.use(routers)
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`)
+  error.status = 404
+  next(error)
+})
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message
+  res.locals.error = process.err.NODE_ENV !== 'production' ? err : {}
+  res.status(err.status || 500);
+  res.render('error')
+})
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중')
+})
+
 
 app.listen(3000);
